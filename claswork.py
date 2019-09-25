ddaae651849fc1691d8f66fff61662a28ad0d2fb
guillermo <porregu@ssfs.org>
2019-09-25 11:49:25 -0400

    Initial commit

---
+++ claswork.py
@@ -0,0 +1,6 @@
+
+def hbd():
+    print("happy birthday to you ")
+print(hbd,(hbd))
+print("happy birthday dear Brian")
+print(hbd)
---
+++ exagon.py
@@ -0,0 +1,28 @@
+
+
+def exagonb():
+    print("  _______")
+    print(" /       \\")
+    print("/         \\")
+
+
+def exagont():
+    print("\\         /")
+    print(" \\       /")
+    print("   _____")
+
+
+def bottom():
+    print("_''_'_''_'_''")
+
+
+exagonb()
+exagont()
+bottom()
+exagonb()
+exagont()
+bottom()
+exagont()
+exagonb()
+bottom()
+exagont()
