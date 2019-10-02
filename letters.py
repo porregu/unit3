a696bec05e01b3f22f527fb1d9be7c0c3ce9ffc3
guillermo <porregu@ssfs.org>
2019-10-02 10:01:47 -0400

    finish unit 3 and class work

--- letters.py
+++ letters.py
@@ -1,5 +1,6 @@
 import turtle
 
+turtle.speed(100)
 def goto(x, y):  # this function makes mobe the pen to a coordenate without showing the line
     turtle.up()
     turtle.goto(x, y)
@@ -15,39 +16,61 @@ def m():
     turtle.fd(25)
     turtle.left(220)
     turtle.fd(50)
+    turtle.seth(0)
+    turtle.seth(0)
 
 
 def i():
-    turtle.left(180)
+    turtle.left(90)
     turtle.fd(50)
-
+    turtle.seth(0)
+    turtle.seth(0)
 
 
 def s():
-    turtle.right(45)
+    turtle.right(300)
     turtle.fd(30)
     turtle.left(120)
     turtle.fd(30)
     turtle.right(120)
     turtle.fd(30)
+    turtle.seth(0)
 
 def p():
-    turtle.right(90)
+    turtle.right(270)
     turtle.fd(50)
     turtle.right(120)
     turtle.fd(20)
     turtle.right(45)
     turtle.fd(20)
+    turtle.left(270)
+    turtle.fd(20)
+    turtle.seth(0)
+
+
 def main():
-    m()
     goto(-200,0)
-    i()
+    m()
     goto(-150,0)
-    s()
+    i()
     goto(-125,0)
     s()
     goto(-100,0)
-    i()
+    s()
     goto(-75,0)
+    i()
+    goto(-50,0)
+    s()
+    goto(-25,0)
+    s()
+    goto(0,0)
+    i()
+    goto(25,0)
+    p()
+    goto(60,0)
+    p()
+    goto(90,0)
+    i()
 
+main()
 turtle.exitonclick()
--- unitproject.py
+++ unitproject.py
@@ -1,21 +1,50 @@
 def arearectangle(a,b):
-   return a*b
+    """
+    solve the area of the rectangle
+    :param a: heigth
+    :param b: with
+    :return: return the fucntion to do it more times
+    """
+    return a*b
 
+def withh():# dpuble (h) because dosent let me put one # with called by the user
+    """
+    make the user tell the with
+    :return: the function so we know the number and we can make the calculation
+    """
+    a=float(input("what is the with"))
+    return a
 
+def lenght():# lenght called by the user
+    """
+    user called the lenght
+    :return: return the function to make the calculations
+    """
+    b=float(input("what is the lenght"))
+    return b
 
-a=7#with
-b=8#length
-c=9#height
+def height():# height called by the user
+    """
+    user called height
+    :return: return the function to make the claculations
+    """
+    c=float(input("what ois the height"))
+    return c
+
+def instructions():
+    print("this is gonna calculate the surace area of a rectangle with the length, with and height choosed by the user")
 
 def calculations():#surface area calculations
-    top=arearectangle(3,4)
-    bottom=arearectangle(3,4)
-    front=arearectangle(9,4)
-    back=arearectangle(9,4)
-    left=arearectangle(9,3)
-    right=arearectangle(9,3)
+    a =withh()
+    b=lenght()
+    c=height()
+    top=arearectangle(a,b)
+    bottom=arearectangle(b,a)
+    front=arearectangle(c,b)
+    back=arearectangle(b,c)
+    left=arearectangle(c,a)
+    right=arearectangle(a,c)
     total=top+bottom+right+left+front+back
+    instructions()
     print(total)
 calculations()
-
-
